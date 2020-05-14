5fa081771ff6850e9a852712dc57b7ec284c9403
olena <olenapatsevko@gmail.com>
2020-05-14 19:27:41 +0300

    5 laba

--- src/main/java/computer/controller/commands/CreateCommand.java
+++ src/main/java/computer/controller/commands/CreateCommand.java
@@ -2,14 +2,17 @@ package computer.controller.commands;
 
 import computer.controller.MainController;
 import computer.entity.Laptop;
+import computer.service.CreateService;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import org.springframework.context.ApplicationContext;
 
 @Data
+@AllArgsConstructor
 public class CreateCommand implements Command {
-
+private CreateService createService;
     @Override
     public void execute(ApplicationContext applicationContext) {
-        applicationContext.getBean(MainController.class).getLaptops().add(applicationContext.getBean(Laptop.class));
+        applicationContext.getBean(MainController.class).getLaptops().add((Laptop) applicationContext.getBean("laptop"));
     }
 }
--- src/main/java/computer/entity/Laptop.java
+++ src/main/java/computer/entity/Laptop.java
@@ -2,7 +2,6 @@ package computer.entity;
 
 import computer.exceptions.annotations.Call;
 import lombok.*;
-import org.springframework.stereotype.Component;
 
 @Data
 @ToString(callSuper = true)
@@ -13,12 +12,21 @@ public class Laptop extends Computer implements Machine {
     private double weight;
     private double battery;
 
-    public Laptop(String name, int processorFrequency, int quantityOfKernel, int computerDataStorage, int computerMemory, double screen, double weight, double battery) {
+    public Laptop(String name,
+                  int processorFrequency,
+                  int quantityOfKernel,
+                  int computerDataStorage,
+                  int computerMemory,
+                  double screen,
+                  double weight,
+                  double battery) {
         super(name, processorFrequency, quantityOfKernel, computerDataStorage, computerMemory);
         this.screen = screen;
         this.weight = weight;
         this.battery = battery;
     }
+
+
 
     @Call
     public void increment() {
